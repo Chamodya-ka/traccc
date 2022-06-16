615a1e9f9e938c199008e10e24502b4fffa7df9a
Chamodya_ka <chamodya@ackcio.com>
2022-06-16 22:24:38 +0530

    shell script starts multiple example jobs

--- multi_process/run_processes.sh
+++ multi_process/run_processes.sh
@@ -11,10 +11,15 @@ do
 done
 echo "number of processes : $num_proc";
 echo "number of events : $events";
-
+export TRACCC_TEST_DATA_DIR='/home/chamodya/myrepos/recent/traccc/data/'
+start=$SECONDS
 for((i=0;i<num_proc;i++))
 do
 	skip=$(($i * $events))
 	echo "Skipping $skip events"
-	# run a traccc_seq_example_cuda job
+	../build/bin/traccc_seq_example_cuda --detector_file=tml_detector/trackml-detector.csv --digitization_config_file=tml_detector/default-geometric-config-generic.json --cell_directory=tml_full/ttbar_mu200/  --events=$events --skip=$skip --input-binary &
+	rm -f performance_track_seeding.root
 done
+wait
+duration=$(( SECONDS - start ));
+echo "Time : $duration";
