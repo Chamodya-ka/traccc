c8d280df8bf8452f4ec7f11d7df67a1acdc6bdf3
Chamodya_ka <chamodya@ackcio.com>
2022-06-20 22:07:55 +0530

    added mps start stop & cpu

---
+++ multi_process/run_processes_cuda.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+num_proc=1
+events=1
+cores=1
+threads=1
+while getopts n:e:c:t: flag;
+do
+    case "${flag}" in
+        n) num_proc=${OPTARG};;
+        e) events=${OPTARG};;
+	c) cores=${OPTARG};;
+	t) threads=${OPTARG};;
+    esac
+done
+echo "number of processes : $num_proc";
+echo "number of events : $events";
+export TRACCC_TEST_DATA_DIR='/home/chamodya/myrepos/recent/traccc/data/'
+start=$SECONDS
+for((i=0;i<num_proc;i++))
+do
+	skip=$(($i * $events))
+	echo "Skipping $skip events";
+	# get processor id	
+	if [ $threads -eq 2 ];then
+		X=$(($i/$cores))
+		Y=$(($X % $threads))
+		
+		if [ $Y -eq 0 ];then
+			p=$((($i % $cores)*2))
+			
+		else
+			p=$((($i % $cores)*2+1))
+		fi
+	fi	
+
+	if [ $threads -eq 1 ];then
+		p=$((($i % $cores)))
+	fi
+	# end get processor id
+	taskset -c $p ../build/bin/traccc_seq_example_cuda --detector_file=tml_detector/trackml-detector.csv --digitization_config_file=tml_detector/default-geometric-config-generic.json --cell_directory=tml_full/ttbar_mu200/  --events=$events --skip=$skip --input-binary &
+	#cur_pid=$!
+	#echo "$(taskset -pc $cur_pid)"
+	#echo "currentbg :$cur_pid"
+	rm -f performance_track_seeding.root
+done
+wait
+duration=$(( SECONDS - start ));
+echo "Time : $duration";
