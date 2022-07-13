b904f6d030cee31a8f7c87374cd083ce5714b68c
Chamodya_ka <attanayakekavishka@gmail.com>
2022-07-13 21:50:36 +0530

    seq example with bp_mr

--- examples/run/cuda/seq_example_cuda.cpp
+++ examples/run/cuda/seq_example_cuda.cpp
@@ -75,14 +75,15 @@ int seq_run(const traccc::full_tracking_input_config& i_cfg,
     vecmem::host_memory_resource host_mr;
     vecmem::cuda::managed_memory_resource mng_mr;
     vecmem::binary_page_memory_resource bp_mr(mng_mr);
+    
     traccc::clusterization_algorithm ca(host_mr);
     traccc::spacepoint_formation sf(host_mr);
     traccc::seeding_algorithm sa(host_mr);
     traccc::track_params_estimation tp(host_mr);
 
-    traccc::cuda::seeding_algorithm sa_cuda(mng_mr);
-    traccc::cuda::track_params_estimation tp_cuda(mng_mr);
-    traccc::cuda::clusterization_algorithm ca_cuda(mng_mr);
+    traccc::cuda::seeding_algorithm sa_cuda(bp_mr);
+    traccc::cuda::track_params_estimation tp_cuda(bp_mr);
+    traccc::cuda::clusterization_algorithm ca_cuda(bp_mr);
 
     // performance writer
     traccc::seeding_performance_writer sd_performance_writer(
@@ -102,7 +103,7 @@ int seq_run(const traccc::full_tracking_input_config& i_cfg,
         traccc::cell_container_types::host cells_per_event =
             traccc::read_cells_from_event(event, i_cfg.cell_directory,
                                           common_opts.input_data_format,
-                                          surface_transforms, digi_cfg, mng_mr);
+                                          surface_transforms, digi_cfg, bp_mr);
 
         /*time*/ auto end_file_reading_cpu = std::chrono::system_clock::now();
         /*time*/ std::chrono::duration<double> time_file_reading_cpu =
